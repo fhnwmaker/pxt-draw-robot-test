0087928b9ccf51de7d8c9dba9154c2519683d594
Juerg <juerg.luthiger@hfhnw.ch>
2022-10-19 19:10:19 +0200

    added setup command

--- main.ts
+++ main.ts
@@ -1,20 +1,55 @@
 enum OnOff {
-    //%block="on"
-    On = 1,
     //%block="off"
-    Off = 0
+    Off = 0,
+    //%block="on"
+    On = 1
 }
+
+enum Vel {
+    //%block="slow"
+    Slow = 0,
+    //%block="medium"
+    Medium = 1,
+    //%block="fast"
+    Fast = 2
+}
+
+enum Accel {
+    //%block="low"
+    Low = 0,
+    //%block="medium"
+    Medium = 1,
+    //%block="high"
+    High = 2
+}
+
 
 /**
  * Custom blocks
  */
 //% weight=100 color=#ff8800 icon="\uf1fc"
 namespace drawrobot {
+    // IMPORTANT: must correspond to the setting on the arduino
     const I2C_ARDUINO_ADDRESS = 8
+
+    function isRunning(): boolean {
+        let buffer = pins.createBuffer(1);
+        buffer = pins.i2cReadBuffer(I2C_ARDUINO_ADDRESS, 1, false);
+        let status = buffer.getNumber(NumberFormat.UInt8LE, 0);
+        return (status == 1);
+    }
+
+    function wait_until_command_is_finished() {
+        // give command some time; download and startup
+        basic.pause(50);  
+        while (isRunning()) {
+            basic.pause(20);
+        }
+    }
 
     //%block="drive with $left revolutions left, and $right revolutions right"
     export function drive(left: number, right: number) {
-        // IMPORTANT: buffer must correspond to application on arduino
+        // IMPORTANT: buffer must correspond to application on the arduino
         let driveCommand = pins.createBuffer(11);
         driveCommand.setNumber(NumberFormat.UInt8LE, 0, 35);  // 35 => #
         driveCommand.setNumber(NumberFormat.UInt8LE, 1, 77);  // 77 => M
@@ -28,10 +63,7 @@ namespace drawrobot {
             driveCommand,
             false
         )
-        basic.pause(20);
-        while (isRunning()) {
-            basic.pause(20);
-        }
+        wait_until_command_is_finished();
     }
 
     //%block="switch motors to $onoff"
@@ -48,16 +80,24 @@ namespace drawrobot {
             powerCommand,
             false
         )
-        basic.pause(20);
-        while (isRunning()) {
-            basic.pause(20);
-        }
+        wait_until_command_is_finished();
     }
 
-    function isRunning(): boolean {
-        let buffer = pins.createBuffer(1);
-        buffer = pins.i2cReadBuffer(I2C_ARDUINO_ADDRESS, 1, false);
-        let status = buffer.getNumber(NumberFormat.UInt8LE, 0);
-        return (status == 1);
-    }
+    //%block="set speed to $vel and acceleration to %accel"
+    export function setup(vel: Vel, accel: Accel) {
+        // IMPORTANT: buffer must correspond to application on arduino
+        let setupCommand = pins.createBuffer(4);
+        setupCommand.setNumber(NumberFormat.UInt8LE, 0, 35);  // 35 => #
+        setupCommand.setNumber(NumberFormat.UInt8LE, 1, 83);  // 83 => S
+        setupCommand.setNumber(NumberFormat.UInt8LE, 2, vel);
+        setupCommand.setNumber(NumberFormat.UInt8LE, 2, accel);
+        
+        // send commend to the arduino with i2c_address=8
+        pins.i2cWriteBuffer(
+            I2C_ARDUINO_ADDRESS,
+            setupCommand,
+            false
+        )
+    }    
+
 }
